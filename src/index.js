81bea2c38cf387140662e712397f80a28aaa33a4
jaswantbaghel <jaswant.adt@gmail.com>
2024-09-26 17:26:41 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/wrappers/App';
 
-// Add bootstrap just to 
+// Add bootstrap just to check
 import 'bootstrap/dist/css/bootstrap.css';
 
 // Add our style
